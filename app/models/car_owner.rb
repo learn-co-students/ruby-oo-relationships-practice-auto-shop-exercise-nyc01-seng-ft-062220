f1e68d691d97c2a6e442a3057b2c9666d62f86e2
Jeff-Adler <Jeffm.adler@gmail.com>
2020-06-29 14:49:48 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -8,3 +8,11 @@ class CarOwner
 
 
 end
+
+# CarOwner.all Get a list of all owners
+
+# CarOwner#cars Get a list of all the cars that a specific owner has
+
+# CarOwner#mechanics Get a list of all the mechanics that a specific owner goes to
+
+# CarOwner.average_number_of_cars Get the average amount of cars owned for all owners
