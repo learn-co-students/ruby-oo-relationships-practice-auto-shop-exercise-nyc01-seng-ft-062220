0b1cd8eb1ee7eb5c83e2b19a17e9aa86c241db18
yulia <juliana.ny2008@gmail.com>
2020-07-04 22:01:53 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,30 @@
+
 class CarOwner
 
   attr_reader :name
-
+@@all=[]
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all 
+    @@all 
+  end
 
+def cars
+  Car.all.select {|car| car.owner==self}
+end
+
+ def mechanics
+  cars.uniq
+classif=cars.map {|car| car.classification}
+Mechanic.all.select {|mech| classif.include?(mech.specialty)}
+end
+
+def self.average_number_of_cars
+  all_cars=Car.all.length
+  all_owners=all.length
+all_cars/all_owners
+end
 end
